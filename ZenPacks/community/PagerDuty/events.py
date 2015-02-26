bb7639e48f83672118dc9acc0fd51e633765de9f
Joseph Anderson <janderson@agero.com>
2015-02-26 09:59:51 -0600

    version 1.5

--- ZenPacks/community/PagerDuty/events.py
+++ ZenPacks/community/PagerDuty/events.py
@@ -1,7 +1,8 @@
 class PagerDutyPreEventPlugin(object):
     def apply(self, eventProxy, dmd):
         event = eventProxy._zepRawEvent.event
-        #try:
-        #    event.pdServiceKey = device.zPDServiceKey
-        #except:
-        #    event.pdServiceKey = 'nokey'
+        try:
+            event.pdServiceKey = device.zPDServiceKey
+        except:
+            try: event.pdServiceKey = 'nokey'
+            except:  pass
