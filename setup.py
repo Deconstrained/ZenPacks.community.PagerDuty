23cfb5f70fe499a78e562ec4ce53439a379ca257
Joseph Anderson <janderson@agero.com>
2015-03-31 08:48:32 -0500

    changed license to Apache

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@
 NAME = "ZenPacks.community.PagerDuty"
 VERSION = "1.5"
 AUTHOR = "Joseph Anderson"
-LICENSE = "GPLv2+"
+LICENSE = "Apache"
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.community']
 PACKAGES = ['ZenPacks', 'ZenPacks.community', 'ZenPacks.community.PagerDuty']
 INSTALL_REQUIRES = []
